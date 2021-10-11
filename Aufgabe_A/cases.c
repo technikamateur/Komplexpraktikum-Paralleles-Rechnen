b862bca4233d37dea18c69b67d5434ee663677cf
Daniel <daniel@technikamateur.de>
2021-10-11 13:14:37 +0200

    Aufgabe A

---
+++ Aufgabe_A/cases.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+static u_int64_t len_string = 64;
+static char string[64] = "LoL";
+
+/* turns string "string" (with length len_string) to uppercase */
+/* returns 1 if there has been an error, 0 if there has been no error */
+
+int toUppercase(char* string, int len_string){}
+
+/* turns string "string" (with length len_string) to lowercase */
+/* returns 1 if there has been an error, 0 if there has been no error */
+
+int toLowercase(char* string, int len_string){}
+
+/* counts the appearences of character "c" in string "string" (with length len_string) */
+/* returns -1 if there has been an error, and the number of appearences if there has been no error */
+
+int countChar(char* string, int len_string, char c){}
+
+
+int main(int argc, char *argv[])
+{}
