bc711f26bdbcba361a220b83c40ab9a61b05359d
Daniel <daniel@technikamateur.de>
2022-01-18 15:22:08 +0100

    diagrams for C

--- C/analyzer.py
+++ C/analyzer.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import re
+from enum import Enum
 from textwrap import wrap
 
 import matplotlib as mpl
@@ -9,14 +10,13 @@ import numpy as np
 
 
 class Type(Enum):
-    SERIAL = "without SIMD"
-    SIMD = "SIMD"
-    EXTREM = "SIMD + xorshift"
-
+    ASERIAL = "without SIMD"
+    BSIMD = "SIMD"
+    CEXTREM = "SIMD + xorshift"
 
 
 class Bench:
-    def __init__(self, compiler, size):
+    def __init__(self, compiler, size, simd):
         self.compiler = compiler
         self.size = size
         self.init = list()
@@ -24,39 +24,21 @@ class Bench:
         self.repetitions = 100
         self.init_mean = None
         self.calc_mean = None
-        self.type = None
+        self.type = simd
 
     def fetch_data(self, lines):
         init_counter = 0
         calc_counter = 0
         for line in lines:
             line = line.lstrip(' ')
-            if re.match('.*OMP_SCHEDULE', line):
-                line_split = line.split("=")
-                line_split = line_split[1].lstrip(' ')
-                self.schedule = line_split.replace('\'', '')
-            elif re.match('.*OMP_PROC_BIND', line):
-                line_split = line.split("=")
-                line_split = line_split[1].lstrip(' ')
-                self.bind = line_split.replace('\'', '')
-            elif line.startswith("Field initializer took"):
-                if init_counter:
-                    line_split = line.split(' ')
-                    self.init.append(float(line_split[3]))
-                else:
-                    init_counter = True
-            elif line.startswith("Calculation took"):
-                if calc_counter:
-                    line_split = line.split(' ')
-                    self.calc.append(float(line_split[2]))
-                else:
-                    calc_counter = True
-            elif line.startswith('We are doing'):
+            if line.startswith("Field initializer took"):
                 line_split = line.split(' ')
-                self.repetitions = line_split[3]
-            elif line.startswith("Done"):
-                init_counter = False
-                calc_counter = False
+                self.init.append(float(line_split[3]))
+
+            elif line.startswith("Calculation took"):
+                line_split = line.split(' ')
+                self.calc.append(float(line_split[2]))
+
         self.init_mean = np.mean(self.init)
         self.calc_mean = np.mean(self.calc)
 
@@ -65,10 +47,10 @@ class Bench:
 print("Processing .txt files")
 gcc_list = list()
 icc_list = list()
-scheduling_list = list()
+simd = None
 
 # gcc and icc
-for file in glob.glob('./results/*cc_S*_T*.txt'):
+for file in glob.glob('./results/*cc_S*_*.txt'):
     with open(file, "r") as input_file:
         lines = input_file.readlines()
         lines = [line.rstrip() for line in lines]
@@ -76,37 +58,36 @@ for file in glob.glob('./results/*cc_S*_T*.txt'):
         file_name = file_name.split(".", 1)[0]
         file_name = file_name.split("_")
         file_name[1] = file_name[1][1:]
-        file_name[2] = file_name[2][1:]
-        bench = Bench(file_name[0], int(file_name[1]), int(file_name[2]))
+        try:
+            file_name[3]
+            simd = Type['CEXTREM']
+        except IndexError:
+            if file_name[2] == 'serial':
+                simd = Type['ASERIAL']
+            else:
+                simd = Type['BSIMD']
+        bench = Bench(file_name[0], int(file_name[1]), simd)
         if file_name[0] == "gcc":
             gcc_list.append(bench)
         else:
             icc_list.append(bench)
         bench.fetch_data(lines)
 
-for file in glob.glob('./results/icc_scheduling*.txt'):
-    with open(file, "r") as input_file:
-        lines = input_file.readlines()
-        lines = [line.rstrip() for line in lines]
-        bench = Bench("icc", 128, 32)
-        scheduling_list.append(bench)
-        bench.fetch_data(lines)
-
 # create beautiful graphs
 print("Creating beautiful graphs")
 for measurement in [gcc_list, icc_list]:
     measurement.sort(key=lambda x: x.size, reverse=False)
-    sub_list = [measurement[i:i + 6] for i in range(0, len(measurement), 6)]
+    sub_list = [measurement[i:i + 3] for i in range(0, len(measurement), 3)]
 
     # calc time plots
     plt.style.use('ggplot')
-    # sub list contains all 6 thread configs of one size
+    # sub list contains all 3 thread configs of one size
     for s in sub_list:
         x_axes = list()
         y_axes = list()
-        s.sort(key=lambda x: x.threads, reverse=False)
-        for b in s:
-            x_axes.append(b.threads)
+        s.sort(key=lambda x: x.type.name, reverse=False)
+        for b in s[:-1]:
+            x_axes.append(b.type.value)
             y_axes.append(b.calc_mean)
             compiler = b.compiler
         plt.plot(x_axes, y_axes, label=b.size)
@@ -115,17 +96,17 @@ for measurement in [gcc_list, icc_list]:
     plt.rcParams['pgf.texsystem'] = "pdflatex"
     plt.legend(loc='upper right', title="Gameboard size")
     plt.ylabel('time in s')
-    plt.xlabel('number of threads')
+    # plt.xlabel('number of threads')
     # plt.xticks([1, 2, 4, 8, 16, 32])
     plt.yscale("log")
-    plt.xscale("log", base=2)
-    plt.gca().xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
-    plt.gca().xaxis.set_major_formatter(mpl.ticker.EngFormatter(places=0))
+    # plt.xscale("log", base=2)
+    # plt.gca().xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
+    # plt.gca().xaxis.set_major_formatter(mpl.ticker.EngFormatter(places=0))
     plt.gca().yaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
     plt.gca().yaxis.set_major_formatter(mpl.ticker.EngFormatter(places=0))
     plt.gca().yaxis.set_minor_locator(mpl.ticker.LogLocator(base=10, subs='all'))
     plt.gca().grid(True, which="both")
-    plt.title("\n".join(wrap("calculate_next_gen run time in dependence of size and threads", 60)))
+    plt.title("\n".join(wrap("calculate_next_gen() run time in dependence of size", 60)))
     plt.savefig("pics/{}_all_calc.png".format(compiler), dpi=300)
     plt.savefig("pics/{}_all_calc.svg".format(compiler))
     plt.savefig('Bericht/{}_all_calc.pgf'.format(compiler), format='pgf')
@@ -136,9 +117,9 @@ for measurement in [gcc_list, icc_list]:
     for s in sub_list:
         x_axes = list()
         y_axes = list()
-        s.sort(key=lambda x: x.threads, reverse=False)
+        s.sort(key=lambda x: x.type.name, reverse=False)
         for b in s:
-            x_axes.append(b.threads)
+            x_axes.append(b.type.value)
             y_axes.append(b.init_mean)
             compiler = b.compiler
         plt.plot(x_axes, y_axes, label=b.size)
@@ -147,12 +128,12 @@ for measurement in [gcc_list, icc_list]:
     plt.rcParams['pgf.texsystem'] = "pdflatex"
     plt.legend(loc='upper right', title="Gameboard size")
     plt.ylabel('time in s')
-    plt.xlabel('number of threads')
+    # plt.xlabel('number of threads')
     # plt.xticks([1, 2, 4, 8, 16, 32])
     plt.yscale("log")
-    plt.xscale("log", base=2)
-    plt.gca().xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
-    plt.gca().xaxis.set_major_formatter(mpl.ticker.EngFormatter(places=0))
+    # plt.xscale("log", base=2)
+    # plt.gca().xaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
+    # plt.gca().xaxis.set_major_formatter(mpl.ticker.EngFormatter(places=0))
     plt.gca().yaxis.set_major_formatter(mpl.ticker.ScalarFormatter())
     plt.gca().yaxis.set_major_formatter(mpl.ticker.EngFormatter(places=0))
     plt.gca().yaxis.set_minor_locator(mpl.ticker.LogLocator(base=10, subs='all'))
@@ -162,50 +143,3 @@ for measurement in [gcc_list, icc_list]:
     plt.savefig("pics/{}_all_init.svg".format(compiler))
     plt.savefig('Bericht/{}_all_init.pgf'.format(compiler), format='pgf')
     plt.close()
-
-####
-# scheduling
-####
-
-scheduling_list.sort(key=lambda x: x.schedule, reverse=False)
-
-# calc time plots
-plt.style.use('ggplot')
-# sub list contains all 6 thread configs of one size
-for b in scheduling_list:
-    x_axes = [b.schedule if "OMP_STACKSIZE" not in b.schedule else "guided"]
-    y_axes = b.calc_mean
-    compiler = b.compiler
-    plt.bar(x_axes, y_axes)
-
-# generate calc plot
-plt.rcParams['pgf.texsystem'] = "pdflatex"
-plt.ylabel('time in s')
-plt.xlabel('schedule')
-plt.gca().grid(True, which="both")
-plt.title("\n".join(wrap("calculate_next_gen run time in dependence of schedule", 60)))
-plt.savefig("pics/scheduling_calc.png", dpi=300)
-plt.savefig("pics/scheduling_calc.svg")
-plt.savefig('Bericht/scheduling_calc.pgf', format='pgf')
-plt.close()
-
-# now init plot
-plt.style.use('ggplot')
-for b in scheduling_list:
-    x_axes = [b.schedule if "OMP_STACKSIZE" not in b.schedule else "guided"]
-    y_axes = b.init_mean * 1000
-    compiler = b.compiler
-    plt.bar(x_axes, y_axes)
-
-# generate init plot
-plt.rcParams['pgf.texsystem'] = "pdflatex"
-plt.ylabel('time in ms')
-plt.xlabel('schedule')
-plt.gca().grid(True, which="both")
-plt.title("\n".join(wrap("field_initializer run time in dependence of schedule", 60)))
-plt.savefig("pics/scheduling_init.png", dpi=300)
-plt.savefig("pics/scheduling_init.svg")
-plt.savefig('Bericht/scheduling_init.pgf', format='pgf')
-plt.close()
-
-print("Done.")
