c5f93df3a2168e8fd98711413fa011bed30ab90f
Daniel <daniel@technikamateur.de>
2022-02-16 14:09:41 +0100

    neighbour matrix works

--- D/gof_mpi_chessboard.c
+++ D/gof_mpi_chessboard.c
@@ -18,9 +18,7 @@ static char output_fname[255] = "life_";
 // MPI
 // works only for square
 static int rank, cluster;
-static u_int32_t block_rc;
-static u_int32_t blocks_per_side;
-static u_int32_t rank_index;
+static u_int32_t rank_index, block_row, block_col, blocks_per_row, blocks_per_col;
 
 // getopt
 static struct option long_options[] =
@@ -35,111 +33,145 @@ static struct option long_options[] =
 void field_initializer(u_int8_t *state) {
     //fills fields with random numbers 0 = dead, 1 = alive
     unsigned seed = time(0) + rank;
-    for (int i = 1; i < block_rc - 1; i++) {
-        for (int j = 1; j < block_rc - 1; j++) {
-            state[i * block_rc + j] = rand_r(&seed) % 2;
+    for (int i = 1; i < block_row - 1; i++) {
+        for (int j = 1; j < block_col - 1; j++) {
+            state[i * block_col + j] = rand_r(&seed) % 2;
         }
     }
 }
 
+void init_chessboard() {
+    u_int32_t exponent = (u_int32_t) log2((double) cluster);
+    if (exponent & 1) {
+        // ungerade
+        blocks_per_col = pow(2, (exponent / 2));
+        blocks_per_row = blocks_per_row * 2;
+    } else {
+        // gerade
+        blocks_per_row = blocks_per_col = pow(2, (exponent / 2));
+    }
+    block_row = rows / blocks_per_row + 2;
+    block_col = columns / blocks_per_col + 2;
+    return;
+}
+
 void init_neighbour(u_int32_t *neighbour_matrix) {
-    for (int i = 1; i < blocks_per_side + 1; i++) {
-        for (int j = 1; j < blocks_per_side + 1; j++) {
-            neighbour_matrix[i * (blocks_per_side + 2) + j] = (i - 1) * blocks_per_side + (j - 1);
+    int blocks_per_side = 0;
+    u_int32_t neighbour_row = blocks_per_row + 2;
+    u_int32_t neighbour_col = blocks_per_col + 2;
+    for (int i = 1; i < neighbour_row - 1; i++) {
+        for (int j = 1; j < neighbour_col - 1; j++) {
+            neighbour_matrix[i * neighbour_row + j] = (i - 1) * blocks_per_row + (j - 1);
         }
     }
-    for (int i = 1; i < blocks_per_side + 1; i++) {
+    for (int i = 1; i < neighbour_row - 1; i++) {
         //left
-        neighbour_matrix[i * (blocks_per_side + 2)] = neighbour_matrix[(i + 1) * (blocks_per_side + 2) - 2];
+        neighbour_matrix[i * neighbour_col] = neighbour_matrix[(i + 1) * neighbour_col - 2];
         //right
-        neighbour_matrix[(i + 1) * (blocks_per_side + 2) - 1] = neighbour_matrix[i * (blocks_per_side + 2) + 1];
+        neighbour_matrix[(i + 1) * neighbour_col - 1] = neighbour_matrix[i * neighbour_col + 1];
+    }
+    for (int i = 1; i < neighbour_col - 1; i++) {
         //top
-        neighbour_matrix[i] = neighbour_matrix[blocks_per_side * (blocks_per_side + 2) + i];
+        neighbour_matrix[i] = neighbour_matrix[(neighbour_row - 2) * neighbour_col + i];
         //bottom
-        neighbour_matrix[(blocks_per_side + 1) * (blocks_per_side + 2) + i] = neighbour_matrix[(blocks_per_side + 2) +
-                                                                                               i];
+        neighbour_matrix[(neighbour_row - 1) * neighbour_col + i] = neighbour_matrix[neighbour_col + i];
     }
     //top left corner
-    neighbour_matrix[0] = neighbour_matrix[blocks_per_side * (blocks_per_side + 2)];
+    neighbour_matrix[0] = neighbour_matrix[(neighbour_row - 1) * neighbour_col - 2];
     //top right corner
-    neighbour_matrix[blocks_per_side + 1] = neighbour_matrix[(blocks_per_side + 1) * (blocks_per_side + 2) - 1];
+    neighbour_matrix[neighbour_col - 1] = neighbour_matrix[(neighbour_row - 2) * neighbour_col + 1];
     // bottom left
-    neighbour_matrix[(blocks_per_side + 1) * (blocks_per_side + 2)] = neighbour_matrix[blocks_per_side + 2];
+    neighbour_matrix[(neighbour_row - 1) * neighbour_col] = neighbour_matrix[neighbour_col];
     //bottom right
-    neighbour_matrix[(blocks_per_side + 2) * (blocks_per_side + 2) - 1] = neighbour_matrix[(blocks_per_side + 2) + 1];
+    neighbour_matrix[neighbour_row * neighbour_col - 1] = neighbour_matrix[neighbour_col + 1];
+    if (rank == 0) {
+        for (int i = 0; i < neighbour_row; i++) {
+            for (int j = 0; j < neighbour_col; j++) {
+                printf("%d  ", neighbour_matrix[i * neighbour_col + j]);
+            }
+            printf("\n");
+        }
+    }
     return;
 }
 
 void edge_maker(u_int8_t *state, u_int8_t *state_old) {
     // top and bottom + corners
-    for (int i = 1; i < block_rc - 1; i++) {
+    for (int i = 1; i < block_col - 1; i++) {
         u_int8_t sum_of_t_edge = state_old[i - 1] +
                                  state_old[i] +
                                  state_old[i + 1] +
-                                 state_old[block_rc + i - 1] +
-                                 state_old[block_rc + i + 1] +
-                                 state_old[2 * block_rc + i - 1] +
-                                 state_old[2 * block_rc + i] +
-                                 state_old[2 * block_rc + i + 1];
-        state[block_rc + i] = (sum_of_t_edge == 3) | ((sum_of_t_edge == 2) & state_old[block_rc + i]);
+                                 state_old[block_col + i - 1] +
+                                 state_old[block_col + i + 1] +
+                                 state_old[2 * block_col + i - 1] +
+                                 state_old[2 * block_col + i] +
+                                 state_old[2 * block_col + i + 1];
+        state[block_col + i] = (sum_of_t_edge == 3) | ((sum_of_t_edge == 2) & state_old[block_col + i]);
 
-        u_int8_t sum_of_b_edge = state_old[(block_rc - 3) * block_rc + i - 1] +
-                                 state_old[(block_rc - 3) * block_rc + i] +
-                                 state_old[(block_rc - 3) * block_rc + i + 1] +
-                                 state_old[(block_rc - 2) * block_rc + i - 1] +
-                                 state_old[(block_rc - 2) * block_rc + i + 1] +
-                                 state_old[(block_rc - 1) * block_rc + i - 1] +
-                                 state_old[(block_rc - 1) * block_rc + i] +
-                                 state_old[(block_rc - 1) * block_rc + i + 1];
-        state[(block_rc - 2) * block_rc + i] =
-                (sum_of_b_edge == 3) | ((sum_of_b_edge == 2) & state_old[(block_rc - 2) * block_rc + i]);
+        u_int8_t sum_of_b_edge = state_old[(block_col - 3) * block_col + i - 1] +
+                                 state_old[(block_col - 3) * block_col + i] +
+                                 state_old[(block_col - 3) * block_col + i + 1] +
+                                 state_old[(block_col - 2) * block_col + i - 1] +
+                                 state_old[(block_col - 2) * block_col + i + 1] +
+                                 state_old[(block_col - 1) * block_col + i - 1] +
+                                 state_old[(block_col - 1) * block_col + i] +
+                                 state_old[(block_col - 1) * block_col + i + 1];
+        state[(block_col - 2) * block_col + i] =
+                (sum_of_b_edge == 3) | ((sum_of_b_edge == 2) & state_old[(block_col - 2) * block_col + i]);
     }
 
-    for (int i = 2; i < block_rc - 2; i++) {
-        u_int8_t sum_of_l_edge = state_old[(i - 1) * block_rc] +
-                                 state_old[(i - 1) * block_rc + 1] +
-                                 state_old[(i - 1) * block_rc + 2] +
-                                 state_old[i * block_rc] +
-                                 state_old[i * block_rc + 2] +
-                                 state_old[(i + 1) * block_rc] +
-                                 state_old[(i + 1) * block_rc + 1] +
-                                 state_old[(i + 1) * block_rc + 2];
-        state[i * block_rc + 1] = (sum_of_l_edge == 3) | ((sum_of_l_edge == 2) & state_old[i * block_rc + 1]);
+    for (int i = 2; i < block_row - 2; i++) {
+        u_int8_t sum_of_l_edge = state_old[(i - 1) * block_col] +
+                                 state_old[(i - 1) * block_col + 1] +
+                                 state_old[(i - 1) * block_col + 2] +
+                                 state_old[i * block_col] +
+                                 state_old[i * block_col + 2] +
+                                 state_old[(i + 1) * block_col] +
+                                 state_old[(i + 1) * block_col + 1] +
+                                 state_old[(i + 1) * block_col + 2];
+        state[i * block_col + 1] = (sum_of_l_edge == 3) | ((sum_of_l_edge == 2) & state_old[i * block_col + 1]);
 
-        u_int8_t sum_of_r_edge = state_old[i * block_rc - 3] +
-                                 state_old[i * block_rc - 2] +
-                                 state_old[i * block_rc - 1] +
-                                 state_old[(i + 1) * block_rc - 3] +
-                                 state_old[(i + 1) * block_rc - 1] +
-                                 state_old[(i + 2) * block_rc - 3] +
-                                 state_old[(i + 2) * block_rc - 2] +
-                                 state_old[(i + 2) * block_rc - 1];
+        u_int8_t sum_of_r_edge = state_old[i * block_col - 3] +
+                                 state_old[i * block_col - 2] +
+                                 state_old[i * block_col - 1] +
+                                 state_old[(i + 1) * block_col - 3] +
+                                 state_old[(i + 1) * block_col - 1] +
+                                 state_old[(i + 2) * block_col - 3] +
+                                 state_old[(i + 2) * block_col - 2] +
+                                 state_old[(i + 2) * block_col - 1];
 
-        state[(i + 1) * block_rc - 2] =
-                (sum_of_r_edge == 3) | ((sum_of_r_edge == 2) & state_old[(i + 1) * block_rc - 2]);
+        state[(i + 1) * block_col - 2] =
+                (sum_of_r_edge == 3) | ((sum_of_r_edge == 2) & state_old[(i + 1) * block_col - 2]);
     }
     return;
 }
 
-void calculate_next_gen(u_int8_t *state, u_int8_t *state_old) {
+void calculate_next_gen(u_int8_t *state, u_int8_t *state_old, u_int32_t *neighbour_matrix) {
     //i = row, j = column
 
     // edges and corners first
     edge_maker(state, state_old);
     // send everything
+    // top
+    MPI_Status status[8];
+    MPI_Request request[8];
+    MPI_Isend(&state[block_col + 1], block_col - 2, MPI_UINT8_T, neighbour_matrix[rank_index - blocks_per_row], 0,
+              MPI_COMM_WORLD, &request[0]);
+    MPI_Irecv(&state[(block_row - 1) * block_col + 1], block_col - 2, MPI_UINT8_T,
+              neighbour_matrix[rank_index + blocks_per_row], 0, MPI_COMM_WORLD, &request[0]);
     // middle; two because: -1 overlap, -1 edge. Could be calculated here, but for performance done before.
-    for (int i = 2; i < block_rc - 2; i++) {
-        for (int j = 2; j < block_rc - 2; j++) {
+    for (int i = 2; i < block_row - 2; i++) {
+        for (int j = 2; j < block_col - 2; j++) {
             //count up a number (8)
-            u_int8_t sum_of_8 = state_old[(i - 1) * block_rc + (j - 1)] +
-                                state_old[(i - 1) * block_rc + j] +
-                                state_old[(i - 1) * block_rc + (j + 1)] +
-                                state_old[i * block_rc + (j - 1)] +
-                                state_old[i * block_rc + (j + 1)] +
-                                state_old[(i + 1) * block_rc + (j - 1)] +
-                                state_old[(i + 1) * block_rc + j] +
-                                state_old[(i + 1) * block_rc + (j + 1)];
-            state[i * block_rc + j] = (sum_of_8 == 3) | ((sum_of_8 == 2) & state_old[i * block_rc + j]);
+            u_int8_t sum_of_8 = state_old[(i - 1) * block_col + (j - 1)] +
+                                state_old[(i - 1) * block_col + j] +
+                                state_old[(i - 1) * block_col + (j + 1)] +
+                                state_old[i * block_col + (j - 1)] +
+                                state_old[i * block_col + (j + 1)] +
+                                state_old[(i + 1) * block_col + (j - 1)] +
+                                state_old[(i + 1) * block_col + j] +
+                                state_old[(i + 1) * block_col + (j + 1)];
+            state[i * block_col + j] = (sum_of_8 == 3) | ((sum_of_8 == 2) & state_old[i * block_col + j]);
         }
     }
     return;
@@ -219,27 +251,28 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     // arguments
     argments(argc, argv);
+    // init blocks
+    init_chessboard();
     // welcome information
     if (rank == 0) {
         printf("Welcome to the MPI game of life!\n");
         printf("We are running with %d ranks\n", cluster);
         printf("Game size: Columns: %lu, Rows: %lu.\n", columns, rows);
+        printf("Blocks: Columns: %d, Rows: %d.\n", blocks_per_col, blocks_per_row);
+        printf("Block size: Columns: %d, Rows: %d.\n", block_col - 2, block_row - 2);
         if (columns != rows) {
             printf("Warning: Your field is not square.\n");
             exit(2);
         }
         printf("Starting now...\n");
     }
-    // calc number of blocks per side
-    blocks_per_side = (u_int32_t) sqrt((double) cluster);
-    block_rc = rows / blocks_per_side + 2;
     // position in neighbour_matrix
-    rank_index = (rank / blocks_per_side + 1) * (blocks_per_side + 2) + (rank % blocks_per_side + 1);
-    u_int32_t *neighbour_matrix = (u_int32_t *) malloc(
-            (blocks_per_side + 2) * (blocks_per_side + 2) * sizeof(u_int32_t));
+    rank_index = (rank / blocks_per_row + 1) * (blocks_per_row + 2) + (rank % blocks_per_col + 1);
+    u_int32_t *neighbour_matrix = (u_int32_t *) malloc((blocks_per_row + 2) * (blocks_per_col + 2) * sizeof(u_int32_t));
+    init_neighbour(neighbour_matrix);
     // initializing states and pointers
-    u_int8_t *state_1 = (u_int8_t *) malloc(block_rc * block_rc * sizeof(u_int8_t));
-    u_int8_t *state_2 = (u_int8_t *) malloc(block_rc * block_rc * sizeof(u_int8_t));
+    u_int8_t *state_1 = (u_int8_t *) malloc(block_row * block_col * sizeof(u_int8_t));
+    u_int8_t *state_2 = (u_int8_t *) malloc(block_row * block_col * sizeof(u_int8_t));
     u_int8_t *state_in = state_1;
     u_int8_t *state_out = state_2;
     u_int8_t *state_tmp = NULL;
@@ -260,10 +293,11 @@ int main(int argc, char *argv[]) {
         t = clock() - t;
         time_out += ((double) t) / CLOCKS_PER_SEC;
     }
+    MPI_Barrier(MPI_COMM_WORLD);
     //calculation
     for (int i = 0; i < repetitions; i++) {
         t = clock();
-        calculate_next_gen(state_out, state_in);
+        calculate_next_gen(state_out, state_in, neighbour_matrix);
         t = clock() - t;
         time_calc += ((double) t) / CLOCKS_PER_SEC;
         state_tmp = state_in;
